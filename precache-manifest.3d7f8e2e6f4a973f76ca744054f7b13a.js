ab12e351445880e923936cc74fccbf433640c96b
Chris Mayes <coachchristac@gmail.com>
2019-01-22 13:56:21 -0500

    Updates

---
+++ precache-manifest.3d7f8e2e6f4a973f76ca744054f7b13a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74225c1b29c9609512e9b1eb5061fa0b",
+    "url": "/tourney/static/media/background__rose-pattern.74225c1b.jpg"
+  },
+  {
+    "revision": "5223f567962822a9789f",
+    "url": "/tourney/static/js/runtime~main.5223f567.js"
+  },
+  {
+    "revision": "64532f3a29faae4b3578",
+    "url": "/tourney/static/js/main.64532f3a.chunk.js"
+  },
+  {
+    "revision": "0baec4270fc215b85342",
+    "url": "/tourney/static/js/1.0baec427.chunk.js"
+  },
+  {
+    "revision": "64532f3a29faae4b3578",
+    "url": "/tourney/static/css/main.4f6140ab.chunk.css"
+  },
+  {
+    "revision": "822f0acc886dae259f7bdb97cdebfd2d",
+    "url": "/tourney/index.html"
+  }
+];
